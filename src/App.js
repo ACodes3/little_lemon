07e88ab71547fe819896fc83b0fb17b9cc3150c5
Aspasija <aspasija.cvetkoska@result.si>
2025-02-11 22:20:57 +0100

    added material ui navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
+import React from 'react';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import AboutPage from './Pages/AboutPage';
+import ContactPage from './Pages/ContactPage';
+import HomePage from './Pages/HomePage';
+
 function App() {
   return (
-    <div className="App">
-      <h1>React App</h1>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/contact" element={<ContactPage />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import ErrorBoundary from './Components/Errors/ErrorBoundary';
+
+// Create a default theme or customize it if needed
+const theme = createTheme({
+  // You can customize the theme here, e.g., primary colors, typography, etc.
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ErrorBoundary>
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    </ErrorBoundary>
   </React.StrictMode>
 );
