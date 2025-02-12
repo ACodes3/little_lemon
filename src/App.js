f7ebc1c8cac36530483fd0d5b02ab19c641fa9bd
Aspasija <aspasija.cvetkoska@result.si>
2025-02-12 11:20:18 +0100

    added the skeleton for the sub-pages

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import AboutPage from './Pages/AboutPage';
 import ContactPage from './Pages/ContactPage';
 import HomePage from './Pages/HomePage';
+import MenuPage from './Pages/MenuPage';
+import OrderPage from './Pages/OrderPage';
+import ReservationsPage from './Pages/ReservationsPage';
 
 function App() {
   return (
@@ -11,6 +14,9 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/about" element={<AboutPage />} />
+          <Route path="/menu" element={<MenuPage />} />
+          <Route path="/reservations" element={<ReservationsPage />} />
+          <Route path="/order" element={<OrderPage />} />
           <Route path="/contact" element={<ContactPage />} />
         </Routes>
       </div>
